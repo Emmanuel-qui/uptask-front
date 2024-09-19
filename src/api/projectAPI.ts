00d5f8a432f2b68253199fb501ae541c923539e8
Emmanuel-qui <emmanuel.quiroz1223@gmail.com>
2024-09-19 09:09:14 -0600

    Agregue redireccion para cuando se guarde el proyecto

--- src/api/projectAPI.ts
+++ src/api/projectAPI.ts
@@ -5,7 +5,7 @@ export async function createProject(formData: ProjectFormData) {
     
     try {
         const { data } = await api.post('/projects', formData)
-        
+        console.log(data)
     } catch (error) {
         console.error(error)
     }
