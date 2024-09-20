31eb68cb287a89270dd75a91d084f68b25cd5c2f
Emmanuel-qui <emmanuel.quiroz1223@gmail.com>
2024-09-19 20:42:15 -0600

    Cree metodo y estructura html para mostrar proyectos creados

--- src/api/projectAPI.ts
+++ src/api/projectAPI.ts
@@ -1,14 +1,28 @@
 import api from '@/lib/axios'
-import { ProjectFormData } from '@/types/index'
+import { dashboardProjectSchema, ProjectFormData } from '@/types/index'
 import { isAxiosError } from 'axios'
 
 export async function createProject(formData: ProjectFormData) {
-    
     try {
         const { data } = await api.post('/projects', formData)
         return data
     } catch (error) {
-        if(isAxiosError(error) && error.response) {
+        if (isAxiosError(error) && error.response) {
+            console.log(error.response.data)
+            throw new Error(error.response.data.message)
+        }
+    }
+}
+
+export async function getProjects() {
+    try {
+        const { data } = await api('/projects')
+        const response = dashboardProjectSchema.safeParse(data.data)
+        if(response.success) {
+            return response.data
+        }
+    } catch (error) {
+        if (isAxiosError(error) && error.response) {
             console.log(error.response.data)
             throw new Error(error.response.data.message)
         }
